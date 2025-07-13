e9bb5b19427d746f3cf8d002b65a70b8d1106883
Dev-under-training <developeronjobtraining@gmail.com>
2025-07-13 01:45:54 +0000

    q

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import Campaign from "@/pages/Campaign";
 import Security from "@/pages/Security";
-import Advanced from "@/pages/Advanced";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -16,7 +15,6 @@ function Router() {
       <Route path="/campaigns" component={Home} />
       <Route path="/create" component={Campaign} />
       <Route path="/security" component={Security} />
-      <Route path="/advanced" component={Advanced} />
       <Route component={NotFound} />
     </Switch>
   );
